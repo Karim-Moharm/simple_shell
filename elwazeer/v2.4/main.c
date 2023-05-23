c23af94c96a664a1c9a5cfce6dc37e6ab712c9a1
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-05-23 12:19:38 -0700

    update last versions

---
+++ elwazeer/v2.4/main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0 when success
+ */
+int main(int __attribute__ ((unused)) argc, char *av[])
+{
+	char *command = NULL;
+
+	while (1)
+	{
+		print_string("($) ");
+		command = get_command();
+		if (_strcmp("exit\n", command) == 0)
+		{
+			free(command);
+			exit(EXIT_SUCCESS);
+		}
+		execute_shell(command, av);
+		free(command);
+	}
+	free(command);
+	return (0);
+}
