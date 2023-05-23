1e44377c32f26f50186c155390fdc5b4a539563e
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-05-23 15:15:10 -0700

    update checker

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0 when success
+ */
+int main(void)
+{
+	char *command = NULL;
+
+	while (1)
+	{
+		print_string("($) ");
+		command = get_command();
+		if (_strcmp("\n", command) == 0)
+		{
+			free(command);
+			continue;
+		}
+		if (_strcmp("exit\n", command) == 0 || command == NULL)
+		{
+			free(command);
+			exit(EXIT_SUCCESS);
+		}
+		execute_shell(command);
+		free(command);
+	}
+	free(command);
+	return (0);
+}
