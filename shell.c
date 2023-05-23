a45fe65ddbbe21741b7f609b257e8bbadbef2028
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-05-23 15:34:19 -0700

    second_shell

---
+++ shell.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ * main - main function of the shell
+ * @argc: argument counter (unused)
+ * @av: argument vector
+ * @envp: environment variable
+ *
+ * Return:0 on success
+ *
+*/
+
+int main(__attribute__((unused)) int argc, char *av[], char *envp[])
+{
+	char *lineptr = NULL;
+
+	while (1)
+	{
+		lineptr = _getline();
+
+		av = tokenize(lineptr);
+		if (av[0] == NULL)
+		{
+			free(lineptr);
+			free(av);
+			continue;
+		}
+
+
+		if (_strcmp(av[0], "env") == 0)
+		{
+			print_env();
+			free(lineptr);
+			continue;
+		}
+
+		handle_exit(av, lineptr);
+		excute_commands(av, envp, lineptr);
+		free(lineptr);
+	}
+/*	free(lineptr);*/
+	return (0);
+}
