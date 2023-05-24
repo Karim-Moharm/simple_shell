adca59f2a14411a8baa235fc911898f635a84f1f
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-05-24 15:48:06 -0700

    test_v17

--- main.c
+++ main.c
@@ -19,10 +19,14 @@ int main(int __attribute__ ((unused)) ac, char *av[], char **env)
 		}
 		command = get_command();
 		av = split_string(command, " \n\t");
-		if (av[0] == NULL || _strcmp(av[0], "exit") == 0)
+		if (av[0] == NULL)
 		{
-			break;
+			free_2D(av);
+			free(command);
+			continue;
 		}
+		if (_strcmp(av[0], "exit") == 0)
+			break;
 		execute_shell(command, av, env);
 		free(command);
 		free_2D(av);
