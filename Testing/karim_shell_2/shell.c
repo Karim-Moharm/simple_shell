f8a7a7b1766fd5715e5a6044c7e87727f61df45e
Karim-Moharm <karimmoharm222@gmail.com>
2023-05-24 05:08:46 -0700

    test checker

---
+++ Testing/karim_shell_2/shell.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+/**
+ * main - main function of the shell
+ * @argc: argument counter (unused)
+ * @av: argument vector
+ * @envp: environment variable
+ *
+ * Return:0 on success
+ *
+*/
+
+int main(__attribute__((unused)) int argc, char **av, char *envp[])
+{
+	char *lineptr = NULL;
+	size_t n = 0;
+
+	while (1)
+	{
+
+		/*lineptr = _getline();*/
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
+		if (getline(&lineptr, &n, stdin) == -1)
+		{
+			free(lineptr);
+			exit(EXIT_SUCCESS);
+		}
+		if (lineptr == NULL)
+		{
+			exit(EXIT_SUCCESS);
+		}
+
+		av = tokenize(lineptr);
+		if (av[0] == NULL)
+			break;
+
+		if (_strcmp(av[0], "env") == 0)
+		{
+			print_env();
+			free(av);
+	/*		free(lineptr);*/
+			continue;
+		}
+
+		handle_exit(av, lineptr);
+		excute_commands(av, envp, lineptr);
+	}
+	free_2D(av);
+	free(lineptr);
+	return (0);
+}
